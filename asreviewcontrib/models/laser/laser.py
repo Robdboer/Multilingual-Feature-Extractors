fe88d80f850513849c8db64a7f8267f977ecabd0
Robdboer <robbiedboer99@gmail.com>
2023-06-06 23:43:36 +0200

    Update laser.py

--- asreviewcontrib/models/laser/laser.py
+++ asreviewcontrib/models/laser/laser.py
@@ -2,6 +2,8 @@ import numpy as np
 from laserembeddings import Laser
 from langdetect import detect
 from asreview.models.feature_extraction.base import BaseFeatureExtraction
+from pathlib import Path
+import glob
 
 
 class laser(BaseFeatureExtraction):
@@ -33,13 +35,29 @@ class laser(BaseFeatureExtraction):
         num_texts = len(texts)
         print('Setting up multilingual model with laser... This could take a while')
 
-        embeddings = []
-        count = 1
-        for text in texts:
-            language = text.apply(detect)
-            print(language, end='\r')
-            print((count/num_texts)*100, '%', end='\r')
-            embeddings.append(laser.embed_sentences(text, lang=language))
-            count += 1
+        feature_matrices = glob.glob('*.npy')
 
-        return np.concatenate(embeddings)
+        # load model if exists
+        if len(feature_matrices) > 0 and len(feature_matrices) < 5:
+            cache_fp = Path("feature_matrix_" + str(len(feature_matrices)) + ".npy")
+            print('Loading matrix from: ' + str(cache_fp))
+            matrix = np.load(cache_fp)
+            np.save(Path("feature_matrix_" + str(len(feature_matrices)+1) + ".npy"), matrix)
+            return matrix
+        else:
+            embeddings = []
+            count = 0
+            for text in texts:
+                try:
+                    lang = detect(text)[:2]
+                except:
+                    lang = 'en'
+
+                print(lang, (count/num_texts)*100, '%', end='\r')
+                embeddings.append(laser.embed_sentences(text, lang=lang))
+                count += 1
+
+            print('Saving matrix...')
+            np.save(Path("feature_matrix_" + str(1) + ".npy"), np.concatenate(embeddings))
+
+            return np.concatenate(embeddings)
